a728fce720469f4174e803d3f0ff137055f665ef
依知川達也 <ichikawatatsuya@adminnoMacBook-Pro.local>
2018-05-05 23:53:40 +0900

    modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
-var gulp = require("gulp");
+var gulp         = require("gulp");
 // css
-var sass = require("gulp-sass");
+var sass         = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
-var plumber = require("gulp-plumber");
-var sourcemaps = require('gulp-sourcemaps');
+var plumber      = require("gulp-plumber");
+var sourcemaps   = require('gulp-sourcemaps');
 // js
-var browserify = require('browserify');
-var source     = require('vinyl-source-stream');
+var browserify   = require('browserify');
+var source       = require('vinyl-source-stream');
+// liveload
+var sync         = require('browser-sync').create();
+var notify       = require('gulp-notify');
+
+
 gulp.task('sass', function() {
   gulp.src('./src/*.scss')
       .pipe(plumber())
       .pipe(sourcemaps.init())
       .pipe(sass())
+      .pipe(sass({outputStyle: 'expanded'}))
       .pipe(autoprefixer({
           browsers: ['last 2 versions', 'ie >= 8', 'Android >= 4','ios_saf >= 8'],
           cascade: false
@@ -20,6 +26,7 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('./dist'));
 });
 
+// js
 gulp.task('build', function() {
     browserify({
         'entries': ['./src/main.js']
@@ -30,7 +37,24 @@ gulp.task('build', function() {
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', function() {
+// BrowserSyncの設定
+// ブラウザ画面への通知を無効化
+gulp.task('sync', function() {
+  sync.init({
+    server: {
+      baseDir: "./dist",
+      notify: false
+    }
+  });
+});
+
+gulp.task('reload', function() {
+  sync.reload();
+});
+
+gulp.task('watch', function() {
   gulp.watch("./src/*.scss",["sass"]);
   gulp.watch(["./src/*.js", "./src/*.vue"],["build"]);
 });
+
+gulp.task('default', ['sass', 'sync', 'build', 'reload', 'watch']);
